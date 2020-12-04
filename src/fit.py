0145480bb70d4a07a5bcccd8e0fb9277636d8c9e
risto <ristohinno@gmail.com>
2020-12-04 20:48:16 +0200

    small refac

--- src/fit.py
+++ src/fit.py
@@ -1,28 +1,28 @@
 import torch
 
 
-def one_batch(xb, yb, cb, learn):
-    if not cb.begin_batch(xb, yb): return
-    loss = cb.learn.loss_func(cb.learn.model(xb), yb)
-    if not cb.after_loss(loss): return
+def one_batch(xb, yb, learn):
+    if not learn.cb.begin_batch(xb, yb): return
+    loss = learn.cb.learn.loss_func(learn.cb.learn.model(xb), yb)
+    if not learn.cb.after_loss(loss): return
     loss.backward()
-    if cb.after_backward(): cb.learn.opt.step()
-    if cb.after_step(): cb.learn.opt.zero_grad()
+    if learn.cb.after_backward(): learn.cb.learn.opt.step()
+    if learn.cb.after_step(): learn.cb.learn.opt.zero_grad()
 
 
-def all_batches(dl, cb, learn):
+def all_batches(dl, learn):
     for xb, yb in dl:
-        one_batch(xb, yb, cb, learn)
-        if cb.do_stop(): return
+        one_batch(xb, yb, learn)
+        if learn.cb.do_stop(): return
 
 
 def fit(epochs, learn):
     if not learn.cb.begin_fit(): return
     for epoch in range(epochs):
         if not learn.cb.begin_epoch(epoch): continue
-        all_batches(learn.train_dl, learn.cb, learn)  ###
+        all_batches(learn.train_dl, learn)
 
         if learn.cb.begin_validate():
-            with torch.no_grad(): all_batches(learn.valid_dl, learn.cb, learn)
+            with torch.no_grad(): all_batches(learn.valid_dl, learn)
         if learn.cb.do_stop() or not learn.cb.after_epoch(): break
     learn.cb.after_fit()
